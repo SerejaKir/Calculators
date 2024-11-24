91c96675bef28c8c05eebc1a61b495169710f003
Sergey <sergeykirichenko@mail.ru>
2024-11-24 11:56:49 +0300

    First commit

---
+++ multip.go
@@ -0,0 +1,10 @@
+package main
+import "fmt"
+func main(){
+var s1, s2 float32
+fmt.Printf("Enter valuees:\n")
+fmt.Scanf("%v\n",&s1)
+fmt.Scanf("%v\n",&s2)
+fmt.Printf("Difference = %f\n",s1*s2)
+}
+
